1df01ac303be84e0b5c59606e6b545b0c665a07a
LukasZhao <yuhangz243@gmail.com>
2023-02-24 22:03:16 +0000

    First practise

---
+++ Question 1.py
@@ -0,0 +1,47 @@
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode(0)
+        result_tail = result
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = (l1.val if l1 else 0)
+            val2 = (l2.val if l2 else 0)
+            carry, out = divmod(val1+val2 + carry, 10)
+
+            result_tail.next = ListNode(out)
+            result_tail = result_tail.next
+
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+
+        return result.next
+
+#test code following
+# Create two linked lists to test the function
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+# Call the function and store the result in a variable
+sol = Solution()
+result = sol.addTwoNumbers(l1, l2)
+
+# Print the values of the resulting linked list
+while result:
+    print(result.val)
+    result = result.next
